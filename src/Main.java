c362c1c707a00ed673e151f5a8fdda4e10ed921e
markd <dsilvamj@mail.uc.edu>
2022-09-26 22:33:43 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double winterCost = 3421.86;
+        double springCost = 4089.12;
+        double summerCost = 3861.22;
+        double fallCost = 3861.22;
+
+        double yearlyMaintenance = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Your yearly home maintenance cost is: " + yearlyMaintenance);
+    }
+}
+
